00c6f15fb94dd4fb04a52b588417aa0121955411
Daniyal <daniyal12333@hotmail.com>
2016-09-24 03:49:09 -0700

    js

---
+++ prac.js
@@ -0,0 +1,44 @@
+var table;
+table = prompt("table" , "0");
+var a=parseInt(table);
+if (a === a) {
+    a = a*1; 
+    document.write("X 1 = " + a + "\n");
+    if (a === a) {
+        var b = a*2;
+        document.write("X 2 = " + b);
+        if (b=== b) {
+            var c = a *3;
+            document.write("X 3 = " + c);
+            if (b=== b) {
+            var d = a *4;
+            document.write("X 4 = " + d);
+            if (b=== b) {
+            var e = a *5;
+            document.write("X 5 = " + e);
+            if (b=== b) {
+            var f = a *6;
+            document.write("X 6 = " + f);
+            if (b=== b) {
+            var g = a *7;
+            document.write("X 7 = " + g);
+            if (b=== b) {
+            var h = a *8;
+            document.write("X 8 = " + h);
+            if (b=== b) {
+            var i = a *9;
+            document.write("X 9 = " + i);
+            if (b=== b) {
+            var j = a *10;
+            document.write("X 3 = " + j);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+    
